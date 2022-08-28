fe9bf714f8a7f5ebdf621239b7100c6270c74877
Rushil Choksi <55545647+rushilchoksi@users.noreply.github.com>
2022-08-28 14:20:14 +0530

    Equivalence class determination algorithm

--- Minimize.py
+++ Minimize.py
@@ -1,8 +1,58 @@
 import sys
 import json
+import itertools
 from operator import add
 from functools import reduce
 from tabulate import tabulate
+
+def searchElement(searchValue, nestedListData):
+    for rowValue in nestedListData:
+        for columnValue in rowValue:
+            if columnValue in searchValue:
+                return True
+    return False
+
+def mostCommon(listData):
+    tempList, flatList = [], [elemData for subList in listData for elemData in subList]
+    for i in list(set(flatList)):
+        if (flatList.count(i) == flatList.count(max(flatList, key = flatList.count))) and (i not in mainData.get('firstState')):
+            tempList.append(i)
+    return tempList
+
+def createEquivalence(iterationList):
+    currentPassList = []
+    for pairData in iterationList:
+        tempStateList, elemList = [], []
+        if len(pairData) >= 2:
+            for pairValue in list(itertools.combinations(i, 2)):
+                tempListOne, tempListTwo = mainData.get(pairValue[0]), mainData.get(pairValue[1])
+                nfsResult, fsResult = [], []
+                for transitionValue in range(len(tempListOne)):
+                    nfsResult.append(tempListOne[transitionValue] in nonFinalStates and tempListTwo[transitionValue] in nonFinalStates), fsResult.append(tempListOne[transitionValue] in finalStates and tempListTwo[transitionValue] in finalStates)
+
+                if (nfsResult[0] and nfsResult[1]) == True:
+                    tempStateList.append(list(pairValue))
+                elif (fsResult[0] and fsResult[1]) == True:
+                    tempStateList.append(list(pairValue))
+                else:
+                    elemList.append(pairValue)
+
+            if len(elemList) != 0:
+                tempStateList.append(mostCommon(elemList))
+            for pairValue in sorted(tempStateList):
+                if len(pairValue) != 0:
+                    currentPassList.append(pairValue)
+        else:
+            currentPassList.append(i)
+
+    if searchElement(mainData.get('firstState'), currentPassList) != True:
+        currentPassList.append(mainData.get('firstState'))
+
+    resultantPassList = []
+    for i in currentPassList:
+        resultantPassList.append(sorted(i))
+
+    return sorted(resultantPassList)
 
 if len(sys.argv) != 3:
     sys.exit('Usage: python3.9 Minimize.py --file FILE_PATH')
@@ -13,7 +63,7 @@ with open(sys.argv[2], 'r') as jsonFile:
 
 stateTransitionTable = []
 for i in mainData.keys():
-    if i != 'finalState':
+    if i not in ['firstState', 'finalState']:
         stateTransitionTable.append([i, ', '.join(mainData.get(i))])
 
 print(f'DFA: State Transition Table')
@@ -37,9 +87,18 @@ for i in reachableStates.keys():
 print(f'\nDFA: State Transition Table After Removing Unreachable States')
 print(tabulate(reachableStatesTable, headers = ['State', '01'], tablefmt = 'psql'))
 
-nonFinalStates = []
+nonFinalStates, finalStates = [], mainData.get("finalState")
 for i in reachableStates.keys():
     if i not in mainData.get('finalState'):
         nonFinalStates.append(i)
 
-print(f'\nNon-final states: {nonFinalStates}\nFinal states: {mainData.get("finalState")}')
+print(f'\nNon-final states: {nonFinalStates}\nFinal states: {finalStates}')
+
+iterationList, currentPassList, iterationCount = [nonFinalStates, finalStates], [], 1
+firstPassList = createEquivalence(iterationList)
+print(f'\nIteration #{str(iterationCount).zfill(2)}: {firstPassList}')
+
+while firstPassList != currentPassList:
+    iterationCount += 1
+    currentPassList = createEquivalence(firstPassList)
+    print(f'Iteration #{str(iterationCount).zfill(2)}: {firstPassList}')
